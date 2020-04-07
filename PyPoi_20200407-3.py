5f9831677cf5283bb5dfe9bdd7b26eb26332d3b9
legvashkof <viva_oliva@tut.by>
2020-04-07 17:52:29 +0300

    sample0407 correct veriables

--- PyPoi_20200407-3.py
+++ PyPoi_20200407-3.py
@@ -1,18 +1,18 @@
 #Ghost game
 from random import randint
 print('Ghost game')
-feeling_brave = True
+brave = True
 score = 0
-while feeling_brave:
+while brave:
     ghost_door = randint (1, 3)
     print('Three doors ahead...')
     print('A ghost is behind one.')
     print('Which door will you open?')
-    door = input('1, 2, or 3?')
+    door = input('1, 2, or 3? ')
     door_num = int(door)
     if door_num == ghost_door:
         print('GHOST!')
-        feeling_brave = False
+        brave = False
     else:
         print('NO ghost! :((')
         print('You have to try next room.')
